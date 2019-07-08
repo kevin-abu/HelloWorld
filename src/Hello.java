7deca784f06ccf1a25b6c84d17869a35c283e4ed
Kevin Jansen Abu <kevinjansenabu1@gmail.com>
2019-07-08 20:50:02 +0800

    Hello World

---
+++ src/Hello.java
@@ -0,0 +1,9 @@
+/* First Eclipse Project - Hello World*/
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
